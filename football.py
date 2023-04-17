e5be75e4fa403c07ccdf6b865dbcc3ca05514554
muneer78 <muneer@duck.com>
2023-04-17 13:04:56 -0500

    Finished script

--- football.py
+++ football.py
@@ -6,7 +6,7 @@ dfcbs = pd.read_csv('CBSSportsSOS.csv', usecols=['Team', 'CBSRank'])
 dffp = pd.read_csv('FantasyProsOLine.csv', usecols=['Team', 'FPRank'])
 dffbs = pd.read_csv('FBSchedulesSOS.csv', usecols=['Team', 'FBRank'])
 dfpff = pd.read_csv('PFFOLine.csv', usecols=['Team', 'PFFRank'])
-dfplayer = pd.read_csv('Player List.csv'usecols=['RK', 'PLAYER NAME', 'TEAM', 'POS'])
+dfplayer = pd.read_csv('Player List.csv', usecols=['RK', 'PLAYER NAME', 'Team', 'POS'])
 teammap = pd.read_csv('TeamDict.csv', index_col=0, squeeze=True).to_dict()
 
 dfcbs['Team'] = dfcbs['Team'].map(teammap)
@@ -30,27 +30,18 @@ df_oline['OLineRank'] = df_oline.iloc[:, 1:3].mean(axis=1)
 df_sos = dfcbs.merge(dffbs[['Team', 'FBRank']], on=["Team"], how="left")
 df_sos = df_sos.fillna(value=0)
 
-cols = df_sos.columns.drop('Team')
-df_oline[cols] = df_oline[cols].apply(pd.to_numeric, errors='coerce')
-df_oline['OLineRank'] = df_oline.iloc[:, 1:3].mean(axis=1)
+cols2 = df_sos.columns.drop('Team')
+df_sos[cols2] = df_sos[cols2].apply(pd.to_numeric, errors='coerce')
+df_sos['SOSRank'] = df_sos.iloc[:, 1:3].mean(axis=1)
 
-print(df_sos)
+df_players = dfplayer.merge(df_oline[['Team', 'OLineRank']], on=['Team'], how="left").merge(df_sos[['Team', 'SOSRank']], on=['Team'], how="left")
+df_players = df_players.rename(columns = {'RK':'ADP', 'PLAYER NAME':'Name', 'POS':'Position'})
 
-# # cols = ['Rank', 'LaghezzaRank']
-# df1[cols] = df1[cols] = df1[cols].apply(pd.to_numeric, errors='coerce', axis=1)
-#
-# df1 = df1.drop_duplicates(subset=['Player', 'Rank'], keep='last')
-# df1['Total Z-Score'] = df1['Total Z-Score_x'].mask(df1['Total Z-Score_x'].eq(0), df1['Total Z-Score_y'])
-# df1['CombinedRank'] = df1['LaghezzaRank'].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
-# df1.loc[(df1.LaghezzaRank.isnull(),  'LaghezzaRank')] = df1.Rank
-# df1["RankDiff"] = df1["Rank"] - df1["LaghezzaRank"].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
-# df1 = df1.rename(columns = {'Rank':'ADP'})
-# df1.to_csv('fulldraftsheet.csv')
-#
-# df2 = pd.read_csv('fulldraftsheet.csv')
-#
-# columns = ['Player', 'Team_x', 'Total Z-Score', 'ADP', 'LaghezzaRank', 'RankDiff']
-# df2 = pd.DataFrame(df2, columns=columns)
-# df2 = df2.rename(columns = {'Team_x':'Team'})
-# df2['ADP'] = sorted(df2['ADP'], key = float)
-# df2.to_csv('draftsheet.csv')
+cols3 = ['ADP', "OLineRank", 'SOSRank']
+df_players[cols3] = df_players[cols3].apply(pd.to_numeric, errors='coerce')
+df_players['PlayerScore'] = df_players[['ADP', "OLineRank", 'SOSRank']].sum(axis=1)
+sorted = df_players.sort_values(by=['PlayerScore'])
+neworder = ['Name', 'Team', 'Position', 'OLineRank', 'SOSRank', 'PlayerScore']
+sorted = sorted.reindex(columns=neworder)
+
+sorted.to_csv('FantasyFootballRanks.csv', index=False)
