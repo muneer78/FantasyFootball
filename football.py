14f6a7a808cb26d4ed4e457c33e09c0a27b9bbb2
muneer78 <muneer@duck.com>
2023-04-17 10:36:37 -0500

    Finished doing o-line and SOS merges.

--- football.py
+++ football.py
@@ -1,59 +1,56 @@
 import pandas as pd
 import numpy as np
 
-dfath = pd.read_csv('AthleticOLine.csv')
-dfcbs = pd.read_csv('CBSSportsSOS.csv')
-dffp = pd.read_csv('FantasyProsOLine.csv')
-dffbs = pd.read_csv('PFFOLine.csv')
-dffpp = pd.read_csv('fg.csv')
-dfplayer = pd.read_csv('Player List.csv')
+dfath = pd.read_csv('AthleticOLine.csv', usecols=['Team', 'Rank'])
+dfcbs = pd.read_csv('CBSSportsSOS.csv', usecols=['Team', 'CBSRank'])
+dffp = pd.read_csv('FantasyProsOLine.csv', usecols=['Team', 'FPRank'])
+dffbs = pd.read_csv('FBSchedulesSOS.csv', usecols=['Team', 'FBRank'])
+dfpff = pd.read_csv('PFFOLine.csv', usecols=['Team', 'PFFRank'])
+dfplayer = pd.read_csv('Player List.csv'usecols=['RK', 'PLAYER NAME', 'TEAM', 'POS'])
+teammap = pd.read_csv('TeamDict.csv', index_col=0, squeeze=True).to_dict()
 
-dflist = [dfath, dfcbs, dffpp, dffp, dffbs, dfplayer]
+dfcbs['Team'] = dfcbs['Team'].map(teammap)
+dffp['Team'] = dffp['Team'].map(teammap)
+dffbs['Team'] = dffbs['Team'].map(teammap)
+dfpff['Team'] = dfpff['Team'].map(teammap)
+
+dflist = [dfplayer]
 for index in range(len(dflist)):
     dflist[index].replace(r'[^\w\s]|_\*', '', regex=True, inplace = True)
     dflist[index].replace(' Jr', '', regex=True, inplace = True)
     dflist[index].replace(' II', '', regex=True, inplace = True)
 
-dfadp = dfadp.astype(str)
+df_oline = dffp.merge(dfpff[['Team', 'PFFRank']], on=["Team"], how="left").merge(dfath[['Team', 'Rank']], on=["Team"], how="left")
+df_oline = df_oline.fillna(value=0)
 
-func = lambda x: ''.join([i[:3] for i in x.strip().split(' ')])
-dffgpit['Key'] = dffgpit.Name.apply(func)
-dfstuff['Key'] = dfstuff.player_name.apply(func)
-dfadp['Key'] = dfadp.Player.apply(func)
-dfzpit['Key'] = dfzpit.Name.apply(func)
-dfzhit['Key'] = dfzhit.Name.apply(func)
-dffghit['Key'] = dffghit.Name.apply(func)
-dflaghezza['Key'] = dflaghezza.Name.apply(func)
+cols = df_oline.columns.drop('Team')
+df_oline[cols] = df_oline[cols].apply(pd.to_numeric, errors='coerce')
+df_oline['OLineRank'] = df_oline.iloc[:, 1:3].mean(axis=1)
 
-dflist2 = [dffgpit, dfzpit, dfadp, dfstuff]
-for index in range(len(dflist2)):
-    dflist2[index].columns.str.strip()
+df_sos = dfcbs.merge(dffbs[['Team', 'FBRank']], on=["Team"], how="left")
+df_sos = df_sos.fillna(value=0)
 
-dfadp = dfadp.drop(['ESPN','CBS','RTS','NFBC','FT'], axis=1)
+cols = df_sos.columns.drop('Team')
+df_oline[cols] = df_oline[cols].apply(pd.to_numeric, errors='coerce')
+df_oline['OLineRank'] = df_oline.iloc[:, 1:3].mean(axis=1)
 
-df1 = dfadp.merge(dffgpit, on=["Key"], how="left").merge(dfstuff[['Key', 'STUFFplus', 'LOCATIONplus', 'PITCHINGplus']], on=["Key"], how="left").merge(dfzpit[['Key', 'Total Z-Score']], on=["Key"], how="left").merge(dfzhit[['Key', 'Total Z-Score']], on=["Key"], how="left").merge(dffghit, on=["Key"], how="left").merge(dflaghezza[['Key', 'LaghezzaRank']], on=["Key"], how="left")
-df1 = df1.fillna(value=0)
+print(df_sos)
 
-cols = ['Rank', 'LaghezzaRank']
-df1[cols] = df1[cols] = df1[cols].apply(pd.to_numeric, errors='coerce', axis=1)
-
-df1 = df1.drop_duplicates(subset=['Player', 'Rank'], keep='last')
-df1['Total Z-Score'] = df1['Total Z-Score_x'].mask(df1['Total Z-Score_x'].eq(0), df1['Total Z-Score_y'])
-df1['CombinedRank'] = df1['LaghezzaRank'].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
-df1.loc[(df1.LaghezzaRank.isnull(),  'LaghezzaRank')] = df1.Rank
-df1["RankDiff"] = df1["Rank"] - df1["LaghezzaRank"].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
-df1 = df1.rename(columns = {'Rank':'ADP'})
-df1.to_csv('fulldraftsheet.csv')
-
-df2 = pd.read_csv('fulldraftsheet.csv')
-
-columns = ['Player', 'Team_x', 'Total Z-Score', 'ADP', 'LaghezzaRank', 'RankDiff']
-df2 = pd.DataFrame(df2, columns=columns)
-df2 = df2.rename(columns = {'Team_x':'Team'})
-df2['ADP'] = sorted(df2['ADP'], key = float)
-df2.to_csv('draftsheet.csv')
-
-df3 = pd.read_csv('draftsheet.csv', index_col=False)
-columns = ['Player', 'Team']
-df3 = pd.DataFrame(df3, columns=columns)
-df3.to_csv('fantrax.csv')
+# # cols = ['Rank', 'LaghezzaRank']
+# df1[cols] = df1[cols] = df1[cols].apply(pd.to_numeric, errors='coerce', axis=1)
+#
+# df1 = df1.drop_duplicates(subset=['Player', 'Rank'], keep='last')
+# df1['Total Z-Score'] = df1['Total Z-Score_x'].mask(df1['Total Z-Score_x'].eq(0), df1['Total Z-Score_y'])
+# df1['CombinedRank'] = df1['LaghezzaRank'].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
+# df1.loc[(df1.LaghezzaRank.isnull(),  'LaghezzaRank')] = df1.Rank
+# df1["RankDiff"] = df1["Rank"] - df1["LaghezzaRank"].mask(df1['LaghezzaRank'].eq(0), df1['Rank'])
+# df1 = df1.rename(columns = {'Rank':'ADP'})
+# df1.to_csv('fulldraftsheet.csv')
+#
+# df2 = pd.read_csv('fulldraftsheet.csv')
+#
+# columns = ['Player', 'Team_x', 'Total Z-Score', 'ADP', 'LaghezzaRank', 'RankDiff']
+# df2 = pd.DataFrame(df2, columns=columns)
+# df2 = df2.rename(columns = {'Team_x':'Team'})
+# df2['ADP'] = sorted(df2['ADP'], key = float)
+# df2.to_csv('draftsheet.csv')
