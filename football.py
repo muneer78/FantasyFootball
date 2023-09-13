bc35a5228ebefbc02692a3ce2d5633e01079289e
muneer78 <muneer@duck.com>
2023-09-12 20:52:55 -0500

    Created weekly pickups script and refactored draft list script.

--- football.py
+++ football.py
@@ -1,82 +1,70 @@
-"""
-Use pandas to create and work with dataframes
-"""
 import pandas as pd
-import pandas_log
 
-def convert_column_headers_to_proper_case(df_temp):
-    """
-    Define the function to convert column headers to proper case
-    """
-    df_temp.columns = [column.title() for column in df_temp.columns]
+def convert_column_headers_to_proper_case(df):
+    df.columns = [column.title() for column in df.columns]
 
-
-dfpff = pd.read_csv("PFFOLine.csv", usecols=["Team", "PFFRank"])
-dfcbsoline = pd.read_csv("CBSOLine.csv", usecols=["Team", "CBSOLineRank"])
-dfpfn = pd.read_csv("PFNOLine.csv", usecols=["Team", "Rank"])
-dfcbs = pd.read_csv("CBSSportsSOS.csv", usecols=["Team", "CBSRank"])
-dffbs = pd.read_csv("FBSchedulesSOS.csv", usecols=["TEAM", "FBRank"])
-dfpfnsos = pd.read_csv("PFNSOS.csv", usecols=["Team", "PFNSOSRank"])
-dfdk = pd.read_csv("DKSOS.csv", usecols=["Team", "DKRank"])
-dfplayer = pd.read_csv("Player List.csv", usecols=["Rank", "Player", "Team", "POS"])
-df_laghezza = pd.read_csv("laghezzaranks.csv")
-# noinspection PyArgumentList
-teammap = pd.read_csv("TeamDict.csv", index_col=0).squeeze().to_dict()
-
-dataframes = [dfpff, dfpfn, dfcbs, dfcbsoline, dffbs, dfpfnsos, dfdk, dfplayer, df_laghezza]
-
-for df in dataframes:
+def load_and_convert_data(file, columns):
+    df = pd.read_csv(file, usecols=columns)
     convert_column_headers_to_proper_case(df)
+    return df
 
-dfpff["Team"] = dfpff["Team"].map(lambda x: teammap.get(x, x))
-dfcbsoline["Team"] = dfcbsoline["Team"].map(lambda x: teammap.get(x, x))
-dfpfn["Team"] = dfpfn["Team"].map(lambda x: teammap.get(x, x))
-dfcbs["Team"] = dfcbs["Team"].map(lambda x: teammap.get(x, x))
-dffbs["Team"] = dffbs["Team"].map(lambda x: teammap.get(x, x))
-dfpfnsos["Team"] = dfpfnsos["Team"].map(lambda x: teammap.get(x, x))
-dfdk["Team"] = dfdk["Team"].map(lambda x: teammap.get(x, x))
+def map_team_names(df, teammap):
+    df["Team"] = df["Team"].map(lambda x: teammap.get(x, x))
 
-dflist = [dfplayer, df_laghezza]
-for index, df in enumerate(dflist):
-    df.replace(r"[^\w\s]|_\*", "", regex=True, inplace=True)
-    df.replace(" Jr", "", regex=True, inplace=True)
-    df.replace(" II", "", regex=True, inplace=True)
+def clean_player_data(df):
+    df.replace(r"[^\w\s]|_\*| Jr| II", "", regex=True, inplace=True)
 
-df_oline = dfpfn.merge(dfpff[["Team", "Pffrank"]], on=["Team"], how="left").merge(
-    dfcbsoline[["Team", "Cbsolinerank"]], on=["Team"], how="left"
-)
-df_oline = df_oline.fillna(value=0)
+def calculate_average_rank(df, columns, new_column):
+    cols = df.columns.drop("Team")
+    df[cols] = df[cols].apply(pd.to_numeric, errors="coerce")
+    df[new_column] = df.iloc[:, columns].mean(axis=1, numeric_only=True)
 
-cols = df_oline.columns.drop("Team")
-df_oline[cols] = df_oline[cols].apply(pd.to_numeric, errors="coerce")
-df_oline["Olinerank"] = df_oline.iloc[:, 1:3].mean(axis=1, numeric_only=True)
+def merge_and_fill_na(df1, df2, on_column, how_type):
+    merged_df = df1.merge(df2, on=[on_column], how=how_type)
+    return merged_df.fillna(value=0)
 
-df_sos = dfcbs.merge(dffbs[["Team", "Fbrank"]], on=["Team"], how="left").merge(
-    dfdk[["Team", "Dkrank"]], on=["Team"], how="left"
-)
-df_sos = df_sos.fillna(value=0)
+def main():
+    dfpff = load_and_convert_data("PFFOLine.csv", ["Team", "PFFRank"])
+    dfcbsoline = load_and_convert_data("CBSOLine.csv", ["Team", "CBSOLineRank"])
+    dfpfn = load_and_convert_data("PFNOLine.csv", ["Team", "Rank"])
+    dfcbs = load_and_convert_data("CBSSportsSOS.csv", ["Team", "CBSRank"])
+    dffbs = load_and_convert_data("FBSchedulesSOS.csv", ["TEAM", "FBRank"])
+    dfpfnsos = load_and_convert_data("PFNSOS.csv", ["Team", "PFNSOSRank"])
+    dfdk = load_and_convert_data("DKSOS.csv", ["Team", "DKRank"])
+    dfplayer = load_and_convert_data("Player List.csv", ["Rank", "Player", "Team", "POS"])
+    df_laghezza = load_and_convert_data("laghezzaranks.csv", [])
+    teammap = pd.read_csv("TeamDict.csv", index_col=0).squeeze().to_dict()
 
-cols2 = df_sos.columns.drop("Team")
-df_sos[cols2] = df_sos[cols2].apply(pd.to_numeric, errors="coerce")
-df_sos["Sosrank"] = df_sos.iloc[:, 1:3].mean(axis=1, numeric_only=True)
+    dataframes = [dfpff, dfpfn, dfcbs, dfcbsoline, dffbs, dfpfnsos, dfdk, dfplayer, df_laghezza]
 
-df_players = dfplayer.merge(
-    df_oline[["Team", "Olinerank"]], on=["Team"], how="left")\
-    .merge(df_laghezza[['Name', 'Ranking']], left_on='Player', right_on='Name', how='left')\
-    .merge(df_sos[["Team", "Sosrank"]], on=["Team"], how="left")
+    for df in dataframes:
+        map_team_names(df, teammap)
 
-# Drop the 'name' column as it's no longer needed
-df_players.drop(columns=['Name'], inplace=True)
+    clean_player_data(dfplayer)
+    clean_player_data(df_laghezza)
 
-df_players = df_players.rename(
-    columns={"Rank": "ADP", "Player": "Name", "Pos": "Position", "Ranking": "LagRank"}
-)
+    df_oline = merge_and_fill_na(dfpfn, dfpff[["Team", "Pffrank"]], "Team", "left")
+    df_oline = merge_and_fill_na(df_oline, dfcbsoline[["Team", "Cbsolinerank"]], "Team", "left")
+    calculate_average_rank(df_oline, [1, 2], "Olinerank")
 
-cols3 = ["ADP", "Olinerank", "Sosrank"]
-df_players[cols3] = df_players[cols3].apply(pd.to_numeric, errors="coerce")
-df_players["PlayerScore"] = df_players[["ADP", "Olinerank", "Sosrank"]].sum(axis=1)
-sorted = df_players.sort_values(by=["PlayerScore"])
-neworder = ["Name", "Team", "Position", "Olinerank", "Sosrank", "LagRank", "PlayerScore"]
-# sorted = sorted.reindex(columns=neworder)
+    df_sos = merge_and_fill_na(dfcbs, dffbs[["Team", "Fbrank"]], "Team", "left")
+    df_sos = merge_and_fill_na(df_sos, dfdk[["Team", "Dkrank"]], "Team", "left")
+    calculate_average_rank(df_sos, [1, 2], "Sosrank")
 
-sorted.to_csv("FantasyFootballRanks.csv", index=False)
+    df_players = merge_and_fill_na(dfplayer, df_oline[["Team", "Olinerank"]], "Team", "left")
+    df_players = merge_and_fill_na(df_players, df_laghezza[["Name", "Ranking"]], "Player", "left")
+    df_players = merge_and_fill_na(df_players, df_sos[["Team", "Sosrank"]], "Team", "left")
+    df_players.drop(columns=['Name'], inplace=True)
+
+    df_players = df_players.rename(columns={"Rank": "ADP", "Player": "Name", "Pos": "Position", "Ranking": "LagRank"})
+
+    calculate_average_rank(df_players, [1, 3, 4], "PlayerScore")
+
+    sorted_df = df_players.sort_values(by=["PlayerScore"])
+    new_order = ["Name", "Team", "Position", "Olinerank", "Sosrank", "LagRank", "PlayerScore"]
+    sorted_df = sorted_df[new_order]
+
+    sorted_df.to_csv("FantasyFootballRanks.csv", index=False)
+
+if __name__ == "__main__":
+    main()
--- weeklyfb.py
+++ weeklyfb.py
@@ -1,193 +1,50 @@
 import pandas as pd
-from datetime import date, datetime
 
-excluded = pd.read_csv("fbexcluded.csv")
+# Read the data from the CSV file into a dataframe
+df = pd.read_csv("FantasyPros_Fantasy_Football_2023_Offense_Snap_Count_Analysis.csv")
 
-# Load the data
-dfrbdsm_off = pd.read_csv('rbsdm.comstats.csv')
-dfrbdsm_def = pd.read_csv('rbsdm.comstats (1).csv')
-dfrbdsm_qb = pd.read_csv('rbsdm.comstats (2).csv')
-dffbref_off = pd.read_csv('off.csv')
-dffbref_qb = pd.read_csv('qb.csv')
-dffbref_def = pd.read_csv('def.csv')
-dffbref_advdef = pd.read_csv('advdef.csv')
+# Read the "fbexcluded.csv" file into a dataframe
+df_excluded = pd.read_csv("fbexcluded.csv")
 
-today = date.today()
+# Create a list of excluded player names
+excluded_players = df_excluded["PLAYER NAME"].tolist()
 
-def rb(filepath):
-    '''Creates weekly offensive calcs'''
-    df = pd.read_csv(filepath, index_col=["-additional"])
+# Create dataframe 1 where Pos = QB
+df1 = df[df['Pos'] == 'QB']
 
-    df = df.drop(0)
-    filter = df[(df["G"] >= 1)]
-    df = df.applymap(lambda x: x.replace('*', ''))
-    df.rename(columns={"K%-": "K", "BB%-": "BB"}, inplace=True)
-    df.fillna(0)
-    df.reset_index(inplace=True)
-    df = df[~df["-additional"].isin(excluded["PlayerId"])]
+# Create dataframe 2 where Pos is WR, RB, or TE
+df2 = df[df['Pos'].isin(['WR', 'RB', 'TE'])]
 
-    filters = df[
-        (df["Att"] > 8)
-        & (df["Y/G"] > 45)
-        & (df["A/G"] > 9)
-    ].sort_values(by="YScm", ascending=False)
-    return filters
+# Filter out players from df1 and df2 if their names are in the excluded list
+df1_filtered = df1[~df1["Player"].isin(excluded_players)]
+df2_filtered = df2[~df2["Player"].isin(excluded_players)]
 
-def qb(filepath):
-    '''Creates weekly offensive calcs'''
-    df = pd.read_csv(filepath, index_col=["-additional"])
+# Create dataframe 1 where Pos = QB
+df1_filtered = df1_filtered[df1_filtered['Pos'] == 'QB']
 
-    df = df.drop(0)
-    filter = df[(df["G"] >= 1)]
-    df = df.applymap(lambda x: x.replace('*', ''))
-    df.fillna(0)
-    df.reset_index(inplace=True)
-    df = df[~df["-additional"].isin(excluded["PlayerId"])]
+# Create dataframe 2 where Pos is WR, RB, or TE
+df2 = df2_filtered[df2_filtered['Pos'].isin(['WR', 'RB', 'TE'])]
 
-    filters = df[
-        (df["Att"] > 15)
-        & (df["Int%"] <= 2.5 )
-        & (df["ANY/A"] >= 5.5)
-    ].sort_values(by="YScm", ascending=False)
-    return filters
+# Sort both dataframes by "Fantasy Pts" column
+df1_filtered = df1_filtered.sort_values(by="Fantasy Pts", ascending=False)
+df2_filtered = df2_filtered.sort_values(by="Fantasy Pts", ascending=False)
 
-def wr(filepath):
-    '''Creates weekly offensive calcs'''
-    df = pd.read_csv(filepath, index_col=["-additional"])
+# Write df1 to a CSV file with a title
+with open("fbpickups.csv", "w", newline="") as csvfile:
+    # Write title for df1
+    csvfile.write("QBs\n")
+    # Write headers for df1
+    csvfile.write(','.join(df1_filtered.columns) + "\n")
+    # Write data for df1
+    for _, row in df1_filtered.head(10).iterrows():
+        csvfile.write(','.join(map(str, row.values)) + "\n")
 
-    df = df.drop(0)
-    filter = df[(df["G"] >= 1)]
-    df = df.applymap(lambda x: x.replace('*','').replace('%', ''))
-    df.fillna(0)
-    df.reset_index(inplace=True)
-    df = df[~df["-additional"].isin(excluded["PlayerId"])]
-
-    filters = df[
-        (df["Succ%"] >= 50.0)
-        & (df["Y/G"] > 45)
-        & (df["R/G"] > 4)
-        & (df["Ctch%"] > 70.0)
-    ].sort_values(by="YScm", ascending=False)
-    return filters
-
-def te(filepath):
-    '''Creates weekly offensive calcs'''
-    df = pd.read_csv(filepath, index_col=["-additional"])
-
-    df = df.drop(0)
-    filter = df[(df["G"] >= 1)]
-    df.columns = df.columns.str.replace("[+,-,%,*,]", "", regex=True)
-    df = df.applymap(lambda x: x.replace('*', ''))
-    df.rename(columns={"K%-": "K", "BB%-": "BB"}, inplace=True)
-    df.fillna(0)
-    df.reset_index(inplace=True)
-    df = df[~df["-additional"].isin(excluded["PlayerId"])]
-
-    filters = df[
-        (df["wRC"] > 135)
-        & (df["OPS"] > 0.8)
-        & (df["K"] < 100)
-        & (df["BB"] > 100)
-        & (df["Off"] > 3)
-        & (df["Barrel"] > 10)
-    ].sort_values(by="Off", ascending=False)
-    return filters
-
-def defense(filepath):
-    df = pd.read_csv(filepath, index_col=["-additional"])
-
-    df = df.drop(0)
-    filter = df[(df["G"] >= 1)]
-
-    df.columns = df.columns.str.replace("[+,-,%,]", "", regex=True)
-    df = df.applymap(lambda x: x.replace('*', ''))
-    df.fillna(0)
-    df.reset_index(inplace=True)
-    df = df[~df["-additional"].isin(excluded["PlayerId"])]
-
-    filters = df[
-        (df["wRC"] > 115)
-        & (df["OPS"] > 0.8)
-        & (df["K"] < 110)
-        & (df["BB"] > 90)
-        & (df["Off"] > 5)
-        & (df["Barrel"] > 10)
-        & (df["PA"] > 40)
-    ].sort_values(by="Off", ascending=False)
-    return filters
-
-# Preprocess and export the dataframes to Excel workbook sheets
-hitdaywindow = [7, 14]
-pawindow = [40]
-pitchdaywindow = [14, 30]
-ipwindow = [10, 30]
-
-df_list = []
-
-# Initialize a list to keep track of printed titles
-printed_titles = []
-
-with open("weeklyadds.csv", "w+") as f:
-    for w in hitdaywindow:
-        df = hitters_wk_preprocessing(f"fgl_hitters_last_{w}.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"Hitters Last {w} Days"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
-
-    for w in pawindow:
-        df = hitters_pa_preprocessing(f"fgl_hitters_{w}_pa.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"Hitters {w} PA"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
-
-    for w in ipwindow:
-        df = sp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"SP {w} Innings"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
-
-        df = rp_preprocessing(f"fgl_pitchers_{w}_ip.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"RP {w} Innings"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
-
-    for w in pitchdaywindow:
-        df = sp_preprocessing(f"fgl_pitchers_last_{w}.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"Pitchers Last {w} Days"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
-
-        df = rp_preprocessing(f"fgl_pitchers_last_{w}.csv")
-        df = df.sort_values(by="Total Z-Score", ascending=False)
-        df_list.append(df)
-        title = f"RP Last {w} Days"
-        if not df.empty and title not in printed_titles:
-            f.write(f"{title}\n")
-            df.round(2).to_csv(f, index=False)
-            f.write("\n")
-            printed_titles.append(title)
+# Append df2 to the same CSV file with a title and headers
+with open("fbpickups.csv", "a", newline="") as csvfile:
+    # Write title for df2
+    csvfile.write("\n\nWR/RB/TE\n")
+    # Write headers for df2
+    csvfile.write(','.join(df2_filtered.columns) + "\n")
+    # Write data for df2
+    for _, row in df2_filtered.head(50).iterrows():
+        csvfile.write(','.join(map(str, row.values)) + "\n")
